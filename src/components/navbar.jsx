e707395f8ba8c24f2fa84cf1a5727dfd3a79fcea
mounta90 <66538018+mounta90@users.noreply.github.com>
2024-12-31 15:49:00 -0700

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <header className="app-header">
+      <img src="../logo192.png" alt="React Logo" className="react-logo" />
+      <span className="app-title">ReactFacts</span>
+    </header>
+  );
+}
